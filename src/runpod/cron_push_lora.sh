19f3a4cf01be278b56787d4f1757f250847e6463
Akshay Shegaonkar <shegaonkar.akshay2@gmail.com>
2026-07-26 13:49:26 +0530

    feat: add cron job script for automated LORA model uploads

---
+++ src/runpod/cron_push_lora.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+REPO_ID=$1
+LORA_NAME=$2
+LORA_VERSION=$3
+MODEL_NAME=$4
+
+if [[ -z "$REPO_ID" || -z "$LORA_NAME" || $LORA_VERSION || $MODEL_NAME ]]; then
+    echo "Error: Missing arguments."
+    echo "Usage:"
+    echo ""
+    echo "  bash <(curl -fsSL https://raw.githubusercontent.com/Akkisdiary/ai-tools/refs/heads/main/src/runpod/cron_push_lora.sh) <repo_id> <lora_name> <lora_version> <model_name>"
+    exit 1
+fi
+
+nohup bash -c '
+	while true; do
+		echo "===== START $(date "+%Y-%m-%d %H:%M:%S") ====="
+		if hf upload ${REPO_ID} /app/ai-toolkit/output/${LORA_NAME}/ ${LORA_NAME}/${MODEL_NAME}/${LORA_VERSION} --repo-type model; then
+			echo "STATUS: SUCCESS"
+		else 
+			echo "STATUS: FAILED (exit code $?)"
+		fi 
+		echo "===== END $(date "+%Y-%m-%d %H:%M:%S") ====="
+		echo
+		sleep 300
+	done
+' >> /cron_push_lora.log 2>&1 &
